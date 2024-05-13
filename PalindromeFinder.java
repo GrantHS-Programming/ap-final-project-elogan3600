9fc100ff0ed0dbd70aa1f93ead22959700cbafae
elogan3600 <elogan3600@student.pps.net>
2024-05-13 10:00:08 -0700

    gubitn

--- PalindromeFinder.java
+++ PalindromeFinder.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 //Some of this was done at home a month or so ago
 public class PalindromeFinder {
         //private static String[] palindromeArray = {}
-        private static ArrayList<String> palindromeList = new ArrayList<String>();
-        private static ArrayList<String> superPalindromeList = new ArrayList<String>();
+        private static final ArrayList<String> palindromeList = new ArrayList<String>();
+        private static final ArrayList<String> superPalindromeList = new ArrayList<String>();
 
         public static String findPalindromes(int iterations) {
             int next = 0;
@@ -12,6 +12,7 @@ public class PalindromeFinder {
             int dig = 0;
             int dig2 = 0;
             int dig3 = 0;
+            int dig4 = 0;
             for (int j = 0; j < iterations; j++) {
                 strNext = next + "";
                 if (String.valueOf(next).length() == 1) {
@@ -28,7 +29,7 @@ public class PalindromeFinder {
                         strNext = next + "";
                         dig = Integer.valueOf(strNext.substring(0,1));
                         System.out.println("DIG IS " + dig + ", NUM IS " + next);
-                        System.out.println(strNext.substring(0,1));
+                        System.out.println(strNext.charAt(0));
                         System.out.println("Strnext is " + strNext);
                     }
                     else {
@@ -126,22 +127,76 @@ public class PalindromeFinder {
                         dig2 = Integer.valueOf(strNext.substring(1,2));
                         dig3 = Integer.valueOf(strNext.substring(2,3));
                     }
-                    else if (!(next + 11 + "").substring(2,3).equals(dig3 + "") && !(next + 11 + "").substring(1,2).equals(dig2 + "") && (next + 11 + "").substring(0,1).equals(dig + "")) { //try removing nots next time
+                    else if ((!(next + 110 + "").substring(1,2).equals(dig2 + "") && (next + 110 + "").substring(0,1).equals(dig + "")) || next + 11 == 9900000) { //Random exception?
+                        next += 110;
+                        strNext = next + "";
+                        dig = Integer.valueOf(strNext.substring(0,1)); //Maybe don't need all of these changes?
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                    }
+                    else if ((!(next + 11 + "").substring(2, 3).equals(dig3 + "") && !(next + 11 + "").substring(1, 2).equals(dig2 + "")) || !(next + 11 + "").substring(0, 1).equals(dig + "")) {
                         next += 11;
                         strNext = next + "";
                         dig = Integer.valueOf(strNext.substring(0,1)); //Maybe don't need all of these changes?
                         dig2 = Integer.valueOf(strNext.substring(1,2));
                         dig3 = Integer.valueOf(strNext.substring(2,3));
                     }
-                    else if (!(next + 1000 + "").substring(0,1).equals(dig + "")) {
+                    else {
+                        next += 1000;
+                    }
+                }
+                else if (String.valueOf(next).length() == 8) {
+                    if (String.valueOf(next + 1).length() != 8) {
+                        next += 2;
+                        strNext = next + "";
+                        dig = Integer.valueOf(strNext.substring(0,1));
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                        dig4 = Integer.valueOf(strNext.substring(3,4));
+                    }
+                    else if (!(next + 1100 + "").substring(2,3).equals(dig3 + "") && (next + 1100 + "").substring(1,2).equals(dig2 + "") && (next + 1100 + "").substring(0,1).equals(dig + "")) {
+                        next += 1100;
+                        strNext = next + "";
+                        dig = Integer.valueOf(strNext.substring(0,1));
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                    }
+                    else if (!(next + 11 + "").substring(0,1).equals(dig + "")) {
                         next += 11;
                         strNext = next + "";
                         dig = Integer.valueOf(strNext.substring(0,1));
                         dig2 = Integer.valueOf(strNext.substring(1,2));
                         dig3 = Integer.valueOf(strNext.substring(2,3));
                     }
+                    else if (!(next + 110 + "").substring(1,2).equals(dig2 + "")) {
+                        next += 110;
+                        strNext = next + "";
+                        dig = Integer.valueOf(strNext.substring(0,1));
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                    }
                     else {
-                        next += 1000;
+                        next += 11000;
+                    }
+                }
+                else if (String.valueOf(next).length() == 9) {
+                    if (String.valueOf(next + 1).length() != 9) {
+                        next += 2;
+                        strNext = next + "";
+                        dig = Integer.valueOf(strNext.substring(0,1));
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                        dig4 = Integer.valueOf(strNext.substring(3,4));
+                    }
+                    else if (!(next + 11000 + "").substring(3,4).equals(dig4 + "") && (next + 11000 + "").substring(2,3).equals(dig3 + "")) {
+                        next += 11000;
+                        dig = Integer.valueOf(strNext.substring(0,1));
+                        dig2 = Integer.valueOf(strNext.substring(1,2));
+                        dig3 = Integer.valueOf(strNext.substring(2,3));
+                        dig4 = Integer.valueOf(strNext.substring(3,4));
+                    }
+                    else {
+                        next += 10000;
                     }
                 }
                 palindromeList.add(next + "");
@@ -156,7 +211,7 @@ public class PalindromeFinder {
                 int intSquared = Integer.parseInt(palindromeList.get(i)) * Integer.parseInt(palindromeList.get(i));
                 String squared = intSquared + "";
                 String backward = "";
-                for (int j = String.valueOf(squared).length() - 1; j >= 0; j--) {
+                for (int j = squared.length() - 1; j >= 0; j--) {
                     backward += squared.substring(j, j + 1);
                 }
                 if (backward.equals(squared)) {
